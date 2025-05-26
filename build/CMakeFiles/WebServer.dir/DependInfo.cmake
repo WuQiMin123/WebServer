bb99e10faab7ff40c00d52bd9634a4f9f094134f
Wqm <w00dyx@163.com>
2025-05-27 00:37:18 +0800

    Second commit

---
+++ build/CMakeFiles/WebServer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wqm/Desktop/Project/WebServer/src/AsyncServer.cpp" "CMakeFiles/WebServer.dir/src/AsyncServer.cpp.o" "gcc" "CMakeFiles/WebServer.dir/src/AsyncServer.cpp.o.d"
+  "/home/wqm/Desktop/Project/WebServer/src/CServer.cpp" "CMakeFiles/WebServer.dir/src/CServer.cpp.o" "gcc" "CMakeFiles/WebServer.dir/src/CServer.cpp.o.d"
+  "/home/wqm/Desktop/Project/WebServer/src/CSession.cpp" "CMakeFiles/WebServer.dir/src/CSession.cpp.o" "gcc" "CMakeFiles/WebServer.dir/src/CSession.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/WebServer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebServer.dir/src/AsyncServer.cpp.o"
+  "CMakeFiles/WebServer.dir/src/AsyncServer.cpp.o.d"
+  "CMakeFiles/WebServer.dir/src/CServer.cpp.o"
+  "CMakeFiles/WebServer.dir/src/CServer.cpp.o.d"
+  "CMakeFiles/WebServer.dir/src/CSession.cpp.o"
+  "CMakeFiles/WebServer.dir/src/CSession.cpp.o.d"
+  "WebServer"
+  "WebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
